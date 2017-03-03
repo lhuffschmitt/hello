b71f5ab40ae5c0b5cae06a1132f963881de726ea
Laurent HUFFSCHMITT <laurent@laurents-MacBook-Pro-2.local>
2017-03-03 12:32:48 +0100

    Initial commit

---
+++ src/dog.java
@@ -0,0 +1,13 @@
+/**
+ * Created by laurent on 3/3/17.
+ */
+public class dog {
+    int size;
+    string word = "breed";
+    string name;
+
+    void bark () {
+        Systen.out.println ("ruff! ruff");
+    }
+
+}
---
+++ src/maindoc.java
@@ -0,0 +1,10 @@
+/**
+ * Created by laurent on 3/3/17.
+ */
+public class maindoc {
+    public static void main (string[] args) {
+        dog d = new dog();
+        d.size = 40;
+        d.bark();
+    }
+}
